dba847d25fd6fa5173acd651fa166139184536de
Rüveyda Dal <ruveydadal.3@gmail.com>
2022-08-11 16:40:10 +0300

    Update dijital saat.cpp

--- dijital saat.cpp
+++ dijital saat.cpp
@@ -2,35 +2,35 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <time.h>
-int main (void)
-{
+
+int main (void) {
 	
 	int saniye=0,dakika=0,saat=0;
-	while(1)
-	{
+
+	while(1) {
 		
 		printf("\n %02d:%02d:%02d",saat,dakika,saniye);
 		fflush(stdout);
 		saniye++;
-	if(saniye==60)
-	{
+
+	if(saniye == 60) {
+
 		dakika=dakika+1;
 		saniye=0;
 	}
-	if(dakika==60)
-	{
+	if(dakika == 60) {
+
 		saat=saat+1;
 		dakika=0;
 	}
-	if(saat==24)
-	{
+	if(saat = 24) {
+
 		saat=0;
 		dakika=0;
 		saniye=0;
 	}
 	sleep(1);
    } 
-   
-  
    return 0;
+   
 }
